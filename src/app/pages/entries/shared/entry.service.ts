675d907ee0dc016d1b16eaa910ba3857be1cfb27
Marcos Ferreira <marcosradix@gmail.com>
2019-03-12 19:21:59 -0300

    melhorias de OO

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,13 +1,13 @@
-import { flatMap ,map, catchError} from 'rxjs/operators';
+import { flatMap, map, catchError } from 'rxjs/operators';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { CategoryService } from './../../categories/shared/category.service';
 import { EntryModel } from './model/entry.model';
-import { Injectable , Injector} from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { Observable } from "rxjs";
 
 
 
-const urlApi : string = "api/entries";
+const urlApi: string = "api/entries";
 @Injectable({
   providedIn: 'root'
 })
@@ -16,32 +16,25 @@ export class EntryService extends BaseResourceService<EntryModel>{
   constructor(
     protected injector: Injector,
     private categoryService: CategoryService
-    ) { 
+  ) {
     super(injector, urlApi, EntryModel.fromJson);
   }
 
 
   createEntry(entry: EntryModel): Observable<EntryModel> {
+    return this.setCategoryAndSendToServer(entry, super.createResource.bind(this));
+  }
+  updateEntry(entry: EntryModel): Observable<EntryModel> {
+    return this.setCategoryAndSendToServer(entry, super.updateResource.bind(this));
+  }
+
+  private setCategoryAndSendToServer(entry: EntryModel, sendFunc: any): Observable<EntryModel>{
     return this.categoryService.getResourceById(entry.categoryId).pipe(
       flatMap(cat => {
         entry.category = cat;
-
-        return super.createResource(entry);
-      }
-      )
-    );
-
-  }
-
-  updateEntry(entry: EntryModel): Observable<EntryModel> {
-   return this.categoryService.getResourceById(entry.categoryId).pipe(
-      flatMap(cat => {
-        entry.category = cat;
-        
-        return super.updateResource(entry);
-      })
-    );
-
+        return sendFunc(entry);
+      }),  catchError(this.handleErro)
+  )
   }
 
 }
